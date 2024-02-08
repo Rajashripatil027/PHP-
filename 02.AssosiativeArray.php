d5dde0b2ce725983dabf9ccf7150128c240c882b
PATIL RAJASHRI <137038273+Rajashripatil027@users.noreply.github.com>
2024-02-08 17:52:37 +0530

    Add files via upload

---
+++ 02.AssosiativeArray.php
@@ -0,0 +1,7 @@
+<?php 
+$salary = array("Rajashri"=>"50000","Revati"=>"40000","Anuja"=>"30000","Pranali"=>"20000");
+echo"Rajashri Salary :".$salary["Rajashri"]."<br/>";
+echo"Revati Salary :".$salary["Revati"]."<br/>";
+echo"Anuja Salary :".$salary["Anuja"]."<br/>";
+echo"Pranali Salary :".$salary["Pranali"]."<br/>";
+?>
